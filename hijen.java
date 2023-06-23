0b2f714ef6426f082d44eabc7408d58237132e2b
M HAREESH <hareeshreddy.m20@gmail.com>
2023-06-23 18:55:38 +0530

    a file added in hijen

---
+++ hijen.java
@@ -0,0 +1,9 @@
+//a new java program for jenkins
+
+class hijen
+{
+           public static void main(String[] args)
+           {
+                System.out.println("Hi Jenkins ");
+           }
+}
